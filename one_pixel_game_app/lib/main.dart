91ee5dd09457ad329cdbff4902933ff12c5b22d8
KevinKobori <kevin.kobori@hotmail.com>
2023-09-18 15:38:51 -0300

    Migrating to new platform game type

--- one_pixel_game_app/lib/main.dart
+++ one_pixel_game_app/lib/main.dart
@@ -1,109 +1,17 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:flame_audio/flame_audio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:one_pixel_game_app/constants/globals.dart';
-import 'package:one_pixel_game_app/constants/sprite_sheets.dart';
-import 'package:one_pixel_game_app/games/one_pixel_game.dart';
-
-OnePixelGame _onePixelGame = OnePixelGame();
+import 'package:one_pixel_game_app/one_pixel_game_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
 
-  // Pre-load sprite sheets.
-  await SpriteSheets.load();
-
-  await FlameAudio.audioCache.loadAll(
-    [
-      Globals.bgm,
-      Globals.luffyJumpSFX,
-      Globals.luffyRunningSandalsSFX,
-      Globals.pauseSFX,
-      Globals.bumpSFX,
-      Globals.powerUpAppearsSFX,
-      Globals.breakBlockSFX,
-    ],
-  );
-
-  // FlameAudio.bgm.play(Globals.bgm, volume: 0.1);
+  PixelAdventure game = PixelAdventure();
 
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Stack(
-        children: [
-          GameWidget(game: _onePixelGame),
-          Scaffold(
-            backgroundColor: Colors.transparent,
-            // body: SizedBox(
-            //   height: 400,
-            //   width: 400,
-            //   child: Column(
-            //     children: [
-            //       Row(
-            //         children: [
-            //           AspectRatio(
-            //             aspectRatio: 1,
-            //             child: FloatingActionButton.extended(
-            //               onPressed: () {},
-            //               label: Image.asset(
-            //                   'assets/images/luffy/menu/luffy_menu.png'),
-            //             ),
-            //           ),
-            //           AspectRatio(
-            //             aspectRatio: 1,
-            //             child: FloatingActionButton.extended(
-            //               onPressed: () {},
-            //               label: Image.asset(
-            //                   'assets/images/luffy/menu/luffy_menu.png'),
-            //             ),
-            //           ),
-            //         ],
-            //       ),
-            //     ],
-            //   ),
-            // ),
-            appBar: AppBar(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              leading: AspectRatio(
-                aspectRatio: 1,
-                child: FloatingActionButton.extended(
-                  onPressed: () {},
-                  label: Image.asset('assets/images/luffy/menu/luffy_menu.png'),
-                ),
-              ),
-              centerTitle: true,
-              title: FloatingActionButton.extended(
-                onPressed: () {},
-                label: Image.asset('assets/images/luffy/menu/luffy_menu.png'),
-              ),
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: FloatingActionButton.extended(
-                      onPressed: () {},
-                      label:
-                          Image.asset('assets/images/luffy/menu/luffy_menu.png'),
-                    ),
-                  ),
-                ),
-              ],
-              toolbarHeight: 128,
-            ),
-            floatingActionButton: FloatingActionButton.extended(
-              onPressed: () {},
-              label: Image.asset('assets/images/luffy/menu/luffy_menu.png'),
-            ),
-          )
-        ],
-      ),
-    ).animate().fadeIn(
-          delay: const Duration(milliseconds: 50),
-          duration: const Duration(milliseconds: 400),
-        ),
+    GameWidget(game: kDebugMode ? PixelAdventure() : game),
   );
 }
---
+++ one_pixel_game_app/lib/one_pixel_game_app.dart
@@ -0,0 +1,140 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame_audio/flame_audio.dart';
+import 'package:flutter/painting.dart';
+import 'package:one_pixel_game_app/components/jump_button.dart';
+import 'package:one_pixel_game_app/components/level.dart';
+import 'package:one_pixel_game_app/components/player.dart';
+
+class PixelAdventure extends FlameGame
+    with
+        HasKeyboardHandlerComponents,
+        DragCallbacks,
+        HasCollisionDetection,
+        TapCallbacks {
+  PixelAdventure() : super() {
+    // FlameAudio.bgm.stop().then(
+    //       (_) => FlameAudio.bgm.play(PixelAdventure.bgmPath, volume: 0.05),
+    //     );
+  }
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+  late CameraComponent cam;
+  Player player = Player(character: 'Pink Man');
+  late JoystickComponent joystick;
+  bool showControls = false;
+  bool playSounds = true;
+  double soundVolume = 1.0;
+  List<String> levelNames = ['Level-01', 'Level-02'];
+  List<String> levelBGMs = [
+    'bgm/one_piece_ost_the_very_very_strongest_bgm.wav',
+    'bgm/one_piece_ost_the_very_very_strongest_bgm.wav'
+  ];
+  int currentLevelIndex = 0;
+
+  @override
+  FutureOr<void> onLoad() async {
+    // Load all images into cache
+    await images.loadAllImages();
+
+    // Load all audios into cache
+    await FlameAudio.audioCache.loadAll(
+      levelBGMs,
+    );
+
+    _loadLevel();
+
+    if (showControls) {
+      addJoystick();
+      add(JumpButton());
+    }
+
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (showControls) {
+      updateJoystick();
+    }
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      priority: 10,
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Joystick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.horizontalMovement = -1;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.horizontalMovement = 1;
+        break;
+      default:
+        player.horizontalMovement = 0;
+        break;
+    }
+  }
+
+  Future<void> loadNextLevel() async {
+    removeWhere((component) => component is Level);
+
+    if (currentLevelIndex < levelNames.length - 1) {
+      currentLevelIndex++;
+      await _loadLevel();
+    } else {
+      // no more levels
+      currentLevelIndex = 0;
+      await _loadLevel();
+    }
+  }
+
+  Future<void> _loadLevel() async {
+    await Future.delayed(const Duration(seconds: 1), () async {
+      Level world = Level(
+        player: player,
+        levelName: levelNames[currentLevelIndex],
+      );
+
+      cam = CameraComponent.withFixedResolution(
+        world: world,
+        width: 640,
+        height: 360,
+      );
+      cam.viewfinder.anchor = Anchor.topLeft;
+
+      addAll([cam, world]);
+      
+      if (FlameAudio.bgm.isPlaying) {
+        await FlameAudio.bgm.stop();
+      } else {
+        await FlameAudio.bgm.play(levelBGMs[currentLevelIndex], volume: 0.05);
+      }
+    });
+  }
+}
