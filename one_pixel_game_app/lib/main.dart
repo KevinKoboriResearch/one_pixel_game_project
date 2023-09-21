3e34cc185e12a6431d78da61ca88f42fb8e32867
KevinKobori <kevin.kobori@hotmail.com>
2023-09-21 14:48:57 -0300

    Fixed audio problems

--- one_pixel_game_app/lib/main.dart
+++ one_pixel_game_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:one_pixel_game_app/one_pixel_game_app.dart';
 
 void main() async {
@@ -12,6 +13,9 @@ void main() async {
   PixelAdventure game = PixelAdventure();
 
   runApp(
-    GameWidget(game: kDebugMode ? PixelAdventure() : game),
+    GameWidget(game: kDebugMode ? PixelAdventure() : game).animate().fadeIn(
+          delay: const Duration(milliseconds: 50),
+          duration: const Duration(milliseconds: 400),
+        ),
   );
 }
