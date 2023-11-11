738f2b7781778911118b945fe9d4afdf313bcb6a
KevinKobori <kevin.kobori@hotmail.com>
2023-11-11 13:29:38 -0300

    Added new tiles assets

---
+++ one_pixel_game_app/assets/tiles/Pirate Bomb Sets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Pirate Bomb Sets" tilewidth="16" tileheight="16" tilecount="480" columns="24">
+ <image source="../images/Pirate Bomb/Sprites/8-Tile-Sets/Tile-Sets (64-64).png" width="384" height="320"/>
+</tileset>
